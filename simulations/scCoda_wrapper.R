e95402a93e1bece3215238cba59bdfd9d3ac5de3
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-12-05 21:21:58 -0500

    update

--- simulations/scCoda_wrapper.R
+++ simulations/scCoda_wrapper.R
@@ -8,34 +8,44 @@ sccoda_wrapper = function(counts, info, formula, python_path='/usr/local/bin/pyt
 
 	use_python(python_path)
 
-	# Import library
-	scoda = import("sccoda")
-	scoda.utils = import("sccoda.util.cell_composition_data")
-	scoda.analysis = import("sccoda.util.comp_ana")
+	tmp = py_capture_output({
+		# Import library
+		scoda = import("sccoda")
+		scoda.utils = import("sccoda.util.cell_composition_data")
+		scoda.analysis = import("sccoda.util.comp_ana")
 
-	# combine counts and metadata
-	df = cbind(counts, info)
+		# combine counts and metadata
+		df = cbind(counts, info)
 
-	# Create combine dataset
-	data = scoda.utils$from_pandas( r_to_py(as.data.frame(df)), r_to_py(array(all.vars(formula))))
+		# Create combine dataset
+		data = scoda.utils$from_pandas( r_to_py(as.data.frame(df)), r_to_py(array(all.vars(formula))))
 
-	# Initialize analysis
-	init = scoda.analysis$CompositionalAnalysis(data, as.character(formula)[2], colnames(counts)[1])
+		# Initialize analysis
+		init = scoda.analysis$CompositionalAnalysis(data, as.character(formula)[2], colnames(counts)[1])
 
-	# Run MCMC
-	fit.mcmc = init$sample_hmc()
+		# Run MCMC
+		fit.mcmc = init$sample_hmc()
 
-	# Extract results
-	df = fit.mcmc$summary_prepare()
-	res = df[[2]]
+		# Extract results
+		df = fit.mcmc$summary_prepare()
+		res = df[[2]]
 
-	# add variable and response names
-	res$variable = unlist(lapply(all.vars(formula), function(x) rep(x,ncol(counts))))
-	res$response = rep(colnames(counts), length(all.vars(formula)))
-
+		# add variable and response names
+		res$variable = unlist(lapply(all.vars(formula), function(x) rep(x,ncol(counts))))
+		res$response = rep(colnames(counts), length(all.vars(formula)))
+	})
 	res
 }
 
+# library(variancePartition)
+# data(varPartDEdata)
 
+# counts = t(countMatrix[1:3,])
+# info = metadata[,c("Disease", "Sex")]
 
-# res = sccoda_wrapper(counts, info, ~ Age + Height, python_path = "/hpc/users/hoffmg01/.cache/R/basilisk/1.8.0/0/bin/python")
+# # local
+# res = sccoda_wrapper( counts, info, ~ Disease + Sex)
+
+# # Minerva
+# res = sccoda_wrapper( counts, info, ~ Disease + Sex, python_path = "/hpc/users/hoffmg01/.cache/R/basilisk/1.8.0/0/bin/python")
+
