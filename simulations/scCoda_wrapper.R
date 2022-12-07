5e1555c9ac10633c7e9c43e8a79affe59fc1f00b
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-12-07 14:21:58 -0500

    update

--- simulations/scCoda_wrapper.R
+++ simulations/scCoda_wrapper.R
@@ -28,7 +28,7 @@ sccoda_wrapper = function(counts, info, formula, python_path='/usr/local/bin/pyt
 		init = scoda.analysis$CompositionalAnalysis(data, as.character(formula)[2], colnames(counts)[1])
 
 		# Run MCMC
-		fit.mcmc = init$sample_hmc(as.integer(20000), as.integer(5000), verbose=FALSE)
+		fit.mcmc = init$sample_hmc(as.integer(200), as.integer(50), verbose=FALSE)
 
 		# Extract results
 		df = fit.mcmc$summary_prepare()	
